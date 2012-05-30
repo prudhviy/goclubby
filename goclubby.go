66ff7a2462c7564d6ba2455573373288ef3d8935
prudhviy <prudhviy@gmail.com>
2012-05-30 16:57:16 +0530

    added basic server listeners

--- goclubby.go
+++ goclubby.go
@@ -1,9 +1,23 @@
 package main
 
 import (
+	"time"
+	"io"
+	"net/http"
+	"log"
 	"fmt"
 )
 
+func server_init(w http.ResponseWriter, req *http.Request) {
+	fmt.Printf("Request- %s %s\n", req.URL, time.Now())
+	io.WriteString(w, "hello world!\n")
+}
+
 func main() {
-	fmt.Printf("hello world")
+	fmt.Printf("goclubby server running at 0.0.0.0:8000\n")
+	http.HandleFunc("/", server_init)
+	err := http.ListenAndServe("0.0.0.0:8000", nil)
+	if err != nil {
+		log.Fatal("In main(): ", err)
+	}
 }
