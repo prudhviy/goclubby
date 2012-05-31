33f6f25da38aaeb7e2deb9d41cbe596883c280fb
Bhaskar teja Yerneni <prudhviy@gmail.com>
2012-06-01 02:57:58 +0530

    now goclubby can concat files and return. used goroutines and channels

--- goclubby.go
+++ goclubby.go
@@ -2,20 +2,61 @@ package main
 
 import (
     "time"
+    
     "io"
+    "io/ioutil"
     "net/http"
+    
     "log"
     "fmt" 
+    
     "runtime"
     "flag"
 )
 
 var numCores = flag.Int("n", runtime.NumCPU(), "number of CPU cores to use")
 
+var files = [2]string{"tests/a.js", "tests/b.js"}
 
-func server_init(w http.ResponseWriter, req *http.Request) {
+func readResource(path string, out chan string) {
+    
+    buf, err := ioutil.ReadFile(path)
+    if err != nil {
+	   fmt.Printf("In %s file, Error occured\n", path)
+    }
+
+    out <- string(buf)
+}
+
+func concatResource(in chan string, numFiles int) (minifiedFile string) {
+    count := 0
+    var temp string
+
+    for fileContent := range in {
+        temp = fileContent
+        minifiedFile = minifiedFile + temp
+        fmt.Printf("received file: %s\n", temp)
+        count = count + 1
+        if count == numFiles {
+            close(in)
+        }
+    }
+
+    fmt.Printf("end of concatResource\n")
+    return 
+}
+
+func serverInit(w http.ResponseWriter, req *http.Request) {
     fmt.Printf("Request- %s %s\n", req.URL, time.Now())
-    io.WriteString(w, "hello world!\n")
+
+    recv := make(chan string, len(files))
+    for _, path := range files {
+        fmt.Printf("path: %s\n", path)
+        go readResource(path, recv)
+    }
+    minifiedFile := concatResource(recv, len(files))
+    fmt.Printf("finally minifiedFile: %s\n", minifiedFile)
+    io.WriteString(w, minifiedFile + "\n")
 }
 
 func main() {
@@ -26,7 +67,7 @@ func main() {
 
     fmt.Printf("goclubby server running at http://0.0.0.0:8000 on %d CPU cores\n", *numCores)
 
-    http.HandleFunc("/", server_init)
+    http.HandleFunc("/", serverInit)
     err := http.ListenAndServe("0.0.0.0:8000", nil)
     if err != nil {
         log.Fatal("In main(): ", err)
