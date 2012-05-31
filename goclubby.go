1f15ac556b6ad2303e59db565f7a8cae1f5768f4
Bhaskar teja Yerneni <prudhviy@gmail.com>
2012-05-31 23:25:30 +0530

    convert tabs to spaces :|

--- goclubby.go
+++ goclubby.go
@@ -1,34 +1,34 @@
 package main
 
 import (
-	"time"
-	"io"
-	"net/http"
-	"log"
-	"fmt" 
-	"runtime"
-	"flag"
+    "time"
+    "io"
+    "net/http"
+    "log"
+    "fmt" 
+    "runtime"
+    "flag"
 )
 
 var numCores = flag.Int("n", runtime.NumCPU(), "number of CPU cores to use")
 
 
 func server_init(w http.ResponseWriter, req *http.Request) {
-	fmt.Printf("Request- %s %s\n", req.URL, time.Now())
-	io.WriteString(w, "hello world!\n")
+    fmt.Printf("Request- %s %s\n", req.URL, time.Now())
+    io.WriteString(w, "hello world!\n")
 }
 
 func main() {
 
-	// make sure app uses all cores
-	flag.Parse()
-	runtime.GOMAXPROCS(*numCores)
+    // make sure app uses all cores
+    flag.Parse()
+    runtime.GOMAXPROCS(*numCores)
 
-	fmt.Printf("goclubby server running at http://0.0.0.0:8000 on %d CPU cores\n", *numCores)
+    fmt.Printf("goclubby server running at http://0.0.0.0:8000 on %d CPU cores\n", *numCores)
 
-	http.HandleFunc("/", server_init)
-	err := http.ListenAndServe("0.0.0.0:8000", nil)
-	if err != nil {
-		log.Fatal("In main(): ", err)
-	}
+    http.HandleFunc("/", server_init)
+    err := http.ListenAndServe("0.0.0.0:8000", nil)
+    if err != nil {
+        log.Fatal("In main(): ", err)
+    }
 }
