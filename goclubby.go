ee2407fdda72c9cfa41abcd6df016b9a00a45a80
prudhviy <prudhviy@gmail.com>
2012-06-01 20:53:48 +0530

    added closure compiler minification support

--- goclubby.go
+++ goclubby.go
@@ -6,6 +6,7 @@ import (
     "io"
     "io/ioutil"
     "net/http"
+    "os/exec"
     
     "log"
     "fmt" 
@@ -17,26 +18,41 @@ import (
 var numCores = flag.Int("n", runtime.NumCPU(), "number of CPU cores to use")
 
 var files = [2]string{"tests/a.js", "tests/b.js"}
+var basePath = "/home/prudhviy/work/goclubby/"
 
-func readResource(path string, out chan string) {
-    
-    buf, err := ioutil.ReadFile(path)
-    if err != nil {
-	   fmt.Printf("In %s file, Error occured\n", path)
+func readResource(filePath string, out chan string, minify bool) {
+
+    if minify {
+        // get the minified byte data using closure compiler
+        resourceData, err := exec.Command("java", "-jar", "closure/compiler.jar", "--js", filePath ).Output()
+        if err != nil {
+            log.Fatal(err)
+        }
+
+        out <- string(resourceData)
+
+    } else {
+        // read the byte data normally
+        resourceData, err := ioutil.ReadFile(filePath)
+        if err != nil {
+           fmt.Printf("In %s file, Error occured\n", filePath)
+        }
+
+        out <- string(resourceData)
+        
     }
-
-    out <- string(buf)
 }
 
-func concatResource(in chan string, numFiles int) (minifiedFile string) {
+func concatResource(in chan string, numFiles int) (clubbedFile string) {
     count := 0
-    var temp string
 
-    for fileContent := range in {
-        temp = fileContent
-        minifiedFile = minifiedFile + temp
+    for resourceData := range in {
+        clubbedFile = clubbedFile + resourceData
         count = count + 1
+        
         if count == numFiles {
+            // close the channel so that for loop stops 
+            // listening for incoming items and breaks the loop 
             close(in)
         }
     }
@@ -47,13 +63,19 @@ func concatResource(in chan string, numFiles int) (minifiedFile string) {
 func serverInit(w http.ResponseWriter, req *http.Request) {
     fmt.Printf("Request- %s %s\n", req.URL, time.Now())
 
+    // same channel is used for communication between
+    // readResource goroutine and concatResource function
     recv := make(chan string, len(files))
-    for _, path := range files {
-        go readResource(path, recv)
+
+    for _, filePath := range files {
+        // create a goroutine on every I/O operation so that
+        // multiple I/O operations happen in parallel
+        go readResource(basePath + filePath, recv, true)
     }
+
     minifiedFile := concatResource(recv, len(files))
 
-    w.Header().Set("Server", "goclubby")
+    w.Header().Set("Server", "goclubby/0.1")
     io.WriteString(w, minifiedFile + "\n")
 }
 
